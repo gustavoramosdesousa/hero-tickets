17c3a09a3fb420fd47df4e303ffbb590d1df3107
gustavors <gustavogrs@gmail.com>
2023-08-08 14:03:45 -0300

    segurança: apagando senha

--- backend/src/infra/mongoDB.ts
+++ backend/src/infra/mongoDB.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectMongoDB(){
     try {
-        await mongoose.connect('mongodb+srv://gustavors:TE8y0uH1AXE3JpHs@gustavors-mongo.vaktpcb.mongodb.net/hero-tickets');
+        await mongoose.connect('mongodb+srv://<user>:<pass>@gustavors-mongo.vaktpcb.mongodb.net/hero-tickets');
         console.log('ConnectMongoDB:', "Success!");
     } catch (error) {
         console.log('ConnectMongoDB:',error);
